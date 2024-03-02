2d7d9b6955cdf1e7d4acdbb7731e13e7d1d4e109
Md-ShakilHossain <shakilh550@gmail.com>
2024-03-02 14:18:44 +0600

    completed Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,43 +1,74 @@
 
 const Navbar = () => {
+    const navLink = <>
+        <li className="flex gap-3 items-center">
+            <p>Product</p>
+            <select />
+        </li>
+        <li className="flex gap-3 items-center">
+            <p>Solutions</p>
+            <select />
+        </li>
+        <li className="flex gap-3 items-center">
+            <p>Resources</p>
+            <select />
+        </li>
+        <li><a>Enterprise</a></li>
+        <li><a>Pricing</a></li>
+    </>
+
+    const navLink1 = <>
+        <li className="flex gap-2">
+            <img src="/public/lang.png" alt="" className="w-6 h-6" />
+            <p>EN</p>
+        </li>
+        <li>
+            <p>Contact Sales</p>
+        </li>
+        <li>
+            <p>Login</p>
+        </li>
+    </>
     return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
+        <div className="navbar bg-base-100 flex justify-between items-start">
+            <div>
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    <ul tabIndex={0} className="dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {navLink}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+
+                <div className="flex items-end gap-6">
+                    <div>
+                        <img src="/public/miro.png" alt="Miro" className="h-4 md:h-8 w-12 md:w-24" />
+                    </div>
+
+                    <div className="hidden lg:block">
+                        <ul className="flex flex-row gap-4">
+                            {navLink}
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
+
+            <div className="flex items-center gap-5">
+            <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {navLink1}
+                    </ul>
+                </div>
+                <div className="hidden lg:block">
+                        <ul className="flex flex-row gap-4">
+                            {navLink1}
+                        </ul>
+                    </div>
+                <button className="bg-[#4262FF] h-12 w-[152px] text-white md:px-[22px] rounded-md md:rounded-3xl border text-center">Sign up free →</button>
             </div>
         </div>
     );
